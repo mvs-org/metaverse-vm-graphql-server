3364e52e8fc6cacde3bac5b1004204a5a35d55bd
canguruhh <sven@mutzl.org>
2021-06-03 22:31:22 +0200

    reorganize contract data

--- src/models/contracts.model.ts
+++ src/models/contracts.model.ts
@@ -1,11 +1,11 @@
 import { keyBy } from 'lodash'
 
 export const CONTRACTS = [
-    require('../contracts/gene-mst.json'),
-    require('../contracts/wetp-mst.json'),
-    require('../contracts/dna-mst.json'),
-    require('../contracts/usdt-mst.json'),
-    require('../contracts/sks-mst.json'),
+    require('../../contracts/gene-mst.json'),
+    require('../../contracts/wetp-mst.json'),
+    require('../../contracts/dna-mst.json'),
+    require('../../contracts/usdt-mst.json'),
+    require('../../contracts/sks-mst.json'),
 ].map(contract=>({
     ...contract,
     abi: JSON.stringify(contract.abi)
