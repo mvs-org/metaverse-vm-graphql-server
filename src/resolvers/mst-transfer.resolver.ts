f92aa0a06b190dfb34a81270c1efe4882ba796e3
canguruhh <sven@mutzl.org>
2021-06-03 15:30:03 +0200

    case insensitive mst transfer search

--- src/resolvers/mst-transfer.resolver.ts
+++ src/resolvers/mst-transfer.resolver.ts
@@ -10,7 +10,7 @@ export const MSTInfoResolver = (parent: any = {}) => {
   return MST_CONTRACTS_MAP[parent.contractId]
 }
 
-export const MSTTransfersResolver = (parent: {address?: string} = {}, { query = {}, limit, offset, sort, }: { query: any, limit: number, offset: number, sort: 'desc' | 'asc' }) => {
+export const MSTTransfersResolver = (parent: { address?: string } = {}, { query = {}, limit, offset, sort, }: { query: any, limit: number, offset: number, sort: 'desc' | 'asc' }) => {
   const address = query.address || parent.address
   const q = {
     // Only known contract addresses
@@ -34,7 +34,16 @@ export const MSTTransfersResolver = (parent: {address?: string} = {}, { query = 
       }
     }),
   }
-  return LogModel.find(q, {}, { limit: limit || 5, skip: offset || 0, sort: { blockNumber: sort == 'desc' ? -1 : 1 }, lean: 1 })
+  return LogModel.find(q, {}, {
+    limit: limit || 5,
+    skip: offset || 0,
+    sort: { blockNumber: sort == 'desc' ? -1 : 1 },
+    lean: 1,
+    collation: {
+      locale: "en",
+      strength: 2
+    }
+  })
     .then((logs: any) => logs.map((log: any) => ({
       transactionHash: log.transactionHash,
       from: fromLogTopic(log.topics[1]),
