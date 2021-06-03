e89c1e0cd2d0620658d7d4760855981697e9400d
canguruhh <sven@mutzl.org>
2021-06-03 09:39:48 +0200

    fix txs resolver for parent transaction list

--- src/resolvers/txs.resolver.ts
+++ src/resolvers/txs.resolver.ts
@@ -1,8 +1,11 @@
 import { TransactionModel } from '../models/transaction.model'
 
-export const TxsResolver = (parent: {address?: string, hash?: string}, { query, limit, offset, sort, }: { query: any, limit: number, offset: number, sort: 'desc'|'asc' }) => {
+export const TxsResolver = (parent: { address?: string, hash?: string } = {}, { query, limit, offset, sort, }: { query: any, limit: number, offset: number, sort: 'desc' | 'asc' }) => {
+  if(query===undefined){
+    query = {}
+  }
   const address = parent.address || query.address
-  const q = query ? {
+  const q = {
     ...(parent && parent.hash && { blockHash: parent.hash }),
     ...(address && {
       $or: [
@@ -22,6 +25,18 @@ export const TxsResolver = (parent: {address?: string, hash?: string}, { query, 
         ...(query.blockNumber_lte && { $lte: query.blockNumber_lte }),
       }
     }),
-  } : {}
-  return TransactionModel.find(q, {}, { limit: limit || 5, skip: offset || 0, sort: { blockNumber: sort == 'desc' ? -1 : 1 } })
+  }
+  console.log({q})
+  return TransactionModel.find(
+    q,
+    {},
+    {
+      limit: limit || 5,
+      skip: offset || 0,
+      sort: { blockNumber: sort == 'desc' ? -1 : 1 },
+      collation: {
+        locale: 'en',
+        strength: 2
+      }
+    })
 }
